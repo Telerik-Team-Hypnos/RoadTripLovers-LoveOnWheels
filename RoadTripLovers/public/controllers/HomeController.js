e73584b437d9c16024f9f1aa97fe7541f3603aac
georgikostadinov <georgikostadinov@yahoo.com>
2014-10-07 16:34:52 +0300

    public live chat implemented

--- RoadTripLovers/public/controllers/HomeController.js
+++ RoadTripLovers/public/controllers/HomeController.js
@@ -1,8 +1,30 @@
 ﻿'use strict';
 
-appMain.controller('HomeController', function ($scope, $location, AccountService) {
+appMain.controller('HomeController', function ($scope, $location, $log, AccountService, LiveChatResource) {
     AccountService.getAll()
         .then(function(response){
             $scope.users = response;
         });
+
+    LiveChatResource.loadMessages(function(messages){
+        $scope.messages = messages[0];
+        $scope.$apply();
+    });
+
+    LiveChatResource.subscribe(function(messages){
+        $scope.messages = $scope.messages.concat(messages);
+        $scope.$apply();
+    });
+
+    $scope.send = function(){
+
+        var message = {
+            user: AccountService.userData.username || "Anonymous",
+            text: $scope.messageText
+        }
+
+        $scope.messageText='';
+
+        LiveChatResource.sendMessage(message);
+    }
 });
