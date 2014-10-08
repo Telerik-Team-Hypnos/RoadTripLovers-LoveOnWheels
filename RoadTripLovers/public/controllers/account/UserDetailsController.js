6717b1060200303488ccea83414bd7ef88017573
vladislavVI <vladkohackera@gmail.com>
2014-10-08 04:12:35 +0300

    Altered the UserDetailsController
    Started the messages functionality logic. Also, made the user-details
    page truly private.

--- RoadTripLovers/public/controllers/account/UserDetailsController.js
+++ RoadTripLovers/public/controllers/account/UserDetailsController.js
@@ -1,11 +1,18 @@
 'use strict';
 
-appMain.controller('UserDetailsController', function ($scope, $location, $routeParams,  AccountService) {
-
-    if($routeParams.id !== undefined) {
+appMain.controller('UserDetailsController', function ($scope, $location, $routeParams, AccountService) {
+    
+	if($routeParams.id !== undefined && AccountService.userData.isAuth===true) {		
         AccountService.getById($routeParams.id)
             .then(function (response) {
                 $scope.currentUser = response;
+				//messages				
+				AccountService.checkIdentity();								
+				//show messsages form				
+				var currentUserId=response._id;
+				var logedUserId=AccountService.userData.userId;				
+				$scope.isNotMyProfile=(currentUserId!==logedUserId);
+				//
             });
     }else{
         $location.path('/');
