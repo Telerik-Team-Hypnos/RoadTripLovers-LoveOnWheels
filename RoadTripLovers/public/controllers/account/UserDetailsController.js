f1f3eca524144bb1c3094558a28f961e57cc4240
vladislavVI <vladkohackera@gmail.com>
2014-10-08 07:32:34 +0300

    Posting messages is now possible
    Altered the User details controller. Now a user can post messages to
    other users.

--- RoadTripLovers/public/controllers/account/UserDetailsController.js
+++ RoadTripLovers/public/controllers/account/UserDetailsController.js
@@ -1,6 +1,6 @@
 'use strict';
 
-appMain.controller('UserDetailsController', function ($scope, $location, $routeParams, AccountService) {
+appMain.controller('UserDetailsController', function ($scope, $location, $routeParams, AccountService, MessagesResource) {
     
 	if($routeParams.id !== undefined && AccountService.userData.isAuth===true) {		
         AccountService.getById($routeParams.id)
@@ -11,7 +11,24 @@ appMain.controller('UserDetailsController', function ($scope, $location, $routeP
 				//show messsages form				
 				var currentUserId=response._id;
 				var logedUserId=AccountService.userData.userId;				
-				$scope.isNotMyProfile=(currentUserId!==logedUserId);
+				$scope.isMyProfile=(currentUserId===logedUserId);
+				$scope.messages={};
+				$scope.messages.sendMessage=function()
+				{			
+					//do some validation here
+					var title=$scope.messages.title;
+					var body=$scope.messages.body;
+					var req={
+						title:title,
+						body:body,			
+						sender:logedUserId,
+						receiver:currentUserId
+					};
+					MessagesResource.addItem(req).then(function(responce){
+						console.log(responce);
+					});
+					console.log(req);
+				};
 				//
             });
     }else{
