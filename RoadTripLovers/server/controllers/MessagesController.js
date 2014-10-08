03592039db738ff3fb85a4e75fd6266a1d17193d
vladislavVI <vladkohackera@gmail.com>
2014-10-08 07:27:51 +0300

    Created a new Messages controller on the server
    Created and linked a new controller to use for the messaging system.

---
+++ RoadTripLovers/server/controllers/MessagesController.js
@@ -0,0 +1,28 @@
+var Message = require('mongoose').model('Message');
+var User=require('mongoose').model('User');
+
+module.exports = {
+	createItem: function(req, res) {	
+		var now = new Date();		
+		//
+		var data={
+			title:req.body.title,
+			body:req.body.body,
+			postTime:now,
+			sender:req.body.sender,
+			receiver:req.body.receiver
+		};
+		
+		var newItem = new Message(data);
+
+        newItem.save(function(err, item) {
+            if (err) {
+                console.log('Failed to create new item: ' + err);
+                return;
+            }
+
+            res.send('Message sent.');
+        });
+
+    },
+};
--- RoadTripLovers/server/controllers/index.js
+++ RoadTripLovers/server/controllers/index.js
@@ -3,11 +3,13 @@ var countriesController = require('./CountriesController');
 var townsController = require('./TownsController');
 var pagesController = require('./PagesController');
 var tripsController = require('./TripsController');
+var messagesController = require('./MessagesController');
 
 module.exports = {
     users: usersController,
     countries: countriesController,
     towns: townsController,
     pages: pagesController,
-    trips: tripsController
+    trips: tripsController,
+	messages: messagesController
 };
